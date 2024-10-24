5c63889927e97a17cd8cb0b08a3c562177eeb1ff
gustavoyago <145396083+gustavoyago@users.noreply.github.com>
2024-10-24 14:59:54 -0300

    24\10

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'categoria',
-    'pergunta',
-    'resposta'
+    'Filosofia',
+    'De quem é a famosa frase “Penso, logo existo”?',
+    'Sócrates'
 )
 
 criaCartao(
-    'categoria',
-    'pergunta',
-    'resposta'
+    'O que é que é',
+    'O que é, o que é? Feito para andar e não anda.',
+    'A rua.'
 )
 
 criaCartao(
-    'categoria',
-    'pergunta',
-    'resposta'
+    'O que é que é',
+    'O que é, o que é? Não se come, mas é bom para se comer.',
+    'O talher.'
 )
 
 criaCartao(
-    'categoria',
-    'pergunta',
-    'resposta'
+    'O que é que é',
+    'O que é, o que é? Tem no meio do ovo.',
+    'A letra "v".'
 )
